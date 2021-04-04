6cb60b6c4241b12794dd3426e10d2fe1b5e0d2dc
vanius.rb <vanius@gmail.com>
2021-04-04 20:49:34 -0300

    Revert "Changing symbol to i32"
    This reverts commit 0fe1754a2b5a35a680f12932e680ff6896d2ceb4.

--- src/config/symbol_minutes.rs
+++ src/config/symbol_minutes.rs
@@ -2,14 +2,14 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Hash, Eq, PartialEq, PartialOrd, Debug, Clone)]
 pub struct SymbolMinutes {
-    pub symbol: i32,
-    pub minutes: i32,
+    pub symbol: String,
+    pub minutes: u32,
 }
 
 impl SymbolMinutes {
-    pub fn new(symbol: &i32, minutes: &i32) -> Self {
+    pub fn new(symbol: &str, minutes: &u32) -> Self {
         Self {
-            symbol: *symbol,
+            symbol: symbol.into(),
             minutes: *minutes,
         }
     }
