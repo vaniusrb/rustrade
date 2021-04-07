e61372537a41d374228d1d796a85fd6607134e72
vaniusrb <vanius@gmail.com>
2021-04-06 23:18:21 -0300

    Creating stop grain

--- examples/command/command_script_back_test.sh
+++ examples/command/command_script_back_test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cargo +nightly run --release -- -y BTCUSDT -m 15 -s "2020-12-20 00:00:00" -e "2020-12-24 23:45:00" script-back-test --file examples/macd.rhai
+cargo +nightly run --release -- -y BTCUSDT -m 15 -s "2020-11-01 00:00:00" -e "2020-12-15 23:45:00" script-back-test --file examples/macd.rhai
